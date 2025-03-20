015bb9f90b6fc633a8f810dae034e6ebabb5ba4e
Steven Pierre <96037516+kiss42@users.noreply.github.com>
2025-03-20 18:06:23 -0400

    🔧 Fixed TailwindCSS setup in Vite

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,7 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import tailwindcss from "@tailwindcss/vite"; // Ensure it's correctly imported
-
+import { defineConfig } from 'vite'
+import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   plugins: [
-    react(),
-    tailwindcss(), // Correct way to use TailwindCSS with Vite
+    tailwindcss(),
   ],
-  base: "/campaign-website/", // Set the base path for GitHub Pages deployment
-});
+})
