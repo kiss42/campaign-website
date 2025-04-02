552a3f9ec1e66a3dcdf156b5fcf5f8a8d85e7e34
Steven Pierre <96037516+kiss42@users.noreply.github.com>
2025-04-02 15:53:04 -0400

    🛠️ Fix logo path for deployment

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ export default defineConfig({
   plugins: [
     tailwindcss(),
   ],
-  base: '/campaign-website/', // This ensures asset paths resolve correctly on GitHub Pages
+  base: '/', // This ensures asset paths resolve correctly on GitHub Pages
 })
